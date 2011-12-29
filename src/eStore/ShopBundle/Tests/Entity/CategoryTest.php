2a459f54a8ceb6c0504328c622be7de69686aa5f
Marko Jovanovic <markozjovanovic@gmail.com>
2011-12-30 00:25:30 +0100

    Changed tests.

--- src/eStore/ShopBundle/Tests/Entity/CategoryTest.php
+++ src/eStore/ShopBundle/Tests/Entity/CategoryTest.php
@@ -1,9 +1,40 @@
 <?php
 namespace eStore\ShopBundle\Tests\Entity;
 
-use eStore\ShopBundle\Entity\Product;
+use eStore\ShopBundle\Entity\Category;
 
-class ProductTest extends \PHPUnit_Framework_TestCase
+class CategoryTest extends \PHPUnit_Framework_TestCase
 {
+    protected  $category;
     
+    public function setUp()
+    {
+        $this->category = new Category();
+    }
+    
+    public function testGetName()
+    {
+        $this->assertEquals(null, $this->category->getName(),
+                "Method getName in class/entity Category didn't return NULL.");
+    }
+    
+    public function testSetName()
+    {
+        $this->category->setName('test');
+        $this->assertEquals('test', $this->category->getName(), 
+                "Method setName didn't set up name.");
+    }
+    
+    public function testGetDescription()
+    {
+        $this->assertEquals(null, $this->category->getDescription(),
+                "Method getDescription in class/entity Product didn't return NULL.");
+    }
+    
+    public function testSetDescription()
+    {
+        $this->category->setDescription('Test description');
+        $this->assertEquals('Test description', $this->category->getDescription(), 
+                "Method setDescription didn't set up description.");
+    }
 }
--- src/eStore/ShopBundle/Tests/Entity/ProductTest.php
+++ src/eStore/ShopBundle/Tests/Entity/ProductTest.php
@@ -5,173 +5,155 @@ use eStore\ShopBundle\Entity\Product;
 
 class ProductTest extends \PHPUnit_Framework_TestCase
 {
+    protected  $product;
+    
+    public function setUp()
+    {
+        $this->product = new Product();
+    }
+    
     public function testGetName()
     {
-        $product = new Product();
-
-        $this->assertEquals(null, $product->getName(),
+        $this->assertEquals(null, $this->product->getName(),
                 "Method getName in class/entity Product didn't return NULL.");
     }
     
     public function testSetName()
     {
-        $product = new Product();
-        $product->setName('test');
-        $this->assertEquals('test', $product->getName(), 
+        $this->product->setName('test');
+        $this->assertEquals('test', $this->product->getName(), 
                 "Method setName didn't set up name.");
     }
     
     public function testGetDescription()
     {
-        $product = new Product();
-
-        $this->assertEquals(null, $product->getDescription(),
+        $this->assertEquals(null, $this->product->getDescription(),
                 "Method getDescription in class/entity Product didn't return NULL.");
     }
     
     public function testSetDescription()
     {
-        $product = new Product();
-        $product->setDescription('Test description');
-        $this->assertEquals('Test description', $product->getDescription(), 
+        $this->product->setDescription('Test description');
+        $this->assertEquals('Test description', $this->product->getDescription(), 
                 "Method setDescription didn't set up description.");
     }
     
     public function testGetPrice()
     {
-        $product = new Product();
-
-        $this->assertEquals(null, $product->getPrice(),
+        $this->assertEquals(null, $this->product->getPrice(),
                 "Method getPrice in class/entity Product didn't return NULL.");
     }
     
     public function testSetPrice()
     {
-        $product = new Product();
-        $product->setPrice(100.22);
-        $this->assertEquals(100.22, $product->getPrice(), 
+        $this->product->setPrice(100.22);
+        $this->assertEquals(100.22, $this->product->getPrice(), 
                 "Method setPrice didn't set up price.");
     }
 
     public function testGetImage()
     {
-        $product = new Product();
-
-        $this->assertEquals(null, $product->getImage(),
+        $this->assertEquals(null, $this->product->getImage(),
                 "Method getImage in class/entity Product didn't return NULL.");
     }
     
     public function testSetImage()
     {
-        $product = new Product();
-        $product->setImage('image');
-        $this->assertEquals('image', $product->getImage(), 
+        $this->product->setImage('image');
+        $this->assertEquals('image', $this->product->getImage(), 
                 "Method setImage didn't set up name.");
     }
     
     public function testGetSlug()
     {
-        $product = new Product();
-
-        $this->assertEquals(null, $product->getSlug(),
+        $this->assertEquals(null, $this->product->getSlug(),
                 "Method getSlug in class/entity Product didn't return NULL.");
     }
     
     public function testSetSlug()
     {
-        $product = new Product();
-        $product->setSlug('test-test-2');
-        $this->assertEquals('test-test-2', $product->getSlug(), 
+        $this->product->setSlug('test-test-2');
+        $this->assertEquals('test-test-2', $this->product->getSlug(), 
                 "Method setSlug didn't set up slug.");
     }
     
     public function testGetCreated()
     {
-        $product = new Product();
-
-        $this->assertTrue(($product->getCreated() instanceOf \DateTime),
+        $this->assertTrue(($this->product->getCreated() instanceOf \DateTime),
                 "Method getCreated in class/entity Product didn't return DateTime obj.");
     }
     
     public function testSetCreated()
     {
-        $product = new Product();
         $dt = new \DateTime();
-        $product->setCreated($dt);
-        $this->assertEquals($dt, $product->getCreated(), 
+        $this->product->setCreated($dt);
+        $this->assertEquals($dt, $this->product->getCreated(), 
                 "Method setCreated didn't set up created.");
-        $this->assertTrue(($product->getCreated() instanceOf \DateTime),
+        $this->assertTrue(($this->product->getCreated() instanceOf \DateTime),
                 "Method getCreated in class/entity Product didn't return DateTime obj.");
     }
     
     public function testGetUpdated()
     {
-        $product = new Product();
-
-        $this->assertTrue(($product->getUpdated() instanceOf \DateTime),
+        $this->assertTrue(($this->product->getUpdated() instanceOf \DateTime),
                 "Method getUpdated in class/entity Product didn't return DateTime obj.");
     }
     
     public function testSetUpdated()
     {
-        $product = new Product();
         $dt = new \DateTime();
-        $product->setUpdated($dt);
-        $this->assertEquals($dt, $product->getUpdated(), 
+        $this->product->setUpdated($dt);
+        $this->assertEquals($dt, $this->product->getUpdated(), 
                 "Method setUpdated didn't set up created.");
-        $this->assertTrue(($product->getUpdated() instanceOf \DateTime),
+        $this->assertTrue(($this->product->getUpdated() instanceOf \DateTime),
                 "Method getUpdated in class/entity Product didn't return DateTime obj.");
     }
     
     public function testSetCreatedValue()
     {
-        $product = new Product();
-        $product->setCreated(null);
-        $product->setUpdated(null);
-        $product->setCreatedValue();
-        $this->assertFalse(null === $product->getCreated());
-        $this->assertFalse(null === $product->getUpdated());
-        $this->assertTrue(($product->getCreated() instanceOf \DateTime));
-        $this->assertTrue(($product->getUpdated() instanceOf \DateTime));
+        $this->product->setCreated(null);
+        $this->product->setUpdated(null);
+        $this->product->setCreatedValue();
+        $this->assertFalse(null === $this->product->getCreated());
+        $this->assertFalse(null === $this->product->getUpdated());
+        $this->assertTrue(($this->product->getCreated() instanceOf \DateTime));
+        $this->assertTrue(($this->product->getUpdated() instanceOf \DateTime));
     }
     
     public function testSetUpdatedValue()
     {
-        $product = new Product();
-        $product->setCreated(null);
-        $product->setUpdated(null);
-        $product->setUpdatedValue();
-        $this->assertTrue(null === $product->getCreated());
-        $this->assertFalse(null === $product->getUpdated());
-        $this->assertFalse(($product->getCreated() instanceOf \DateTime));
-        $this->assertTrue(($product->getUpdated() instanceOf \DateTime));
+        $this->product->setCreated(null);
+        $this->product->setUpdated(null);
+        $this->product->setUpdatedValue();
+        $this->assertTrue(null === $this->product->getCreated());
+        $this->assertFalse(null === $this->product->getUpdated());
+        $this->assertFalse(($this->product->getCreated() instanceOf \DateTime));
+        $this->assertTrue(($this->product->getUpdated() instanceOf \DateTime));
     }
     
     public function testGetId()
     {
-        $product = new Product();
-        $this->assertTrue(null === $product->getId());
+        $this->assertTrue(null === $this->product->getId());
     }
     
     public function testToString()
     {
-        $product = new Product();
-        $this->assertTrue(($product->getCreated() instanceOf \DateTime));
-        $product->setName('test');
-        $this->assertTrue('test' == $product->__toString());
+        $this->assertTrue(($this->product->getCreated() instanceOf \DateTime));
+        $this->product->setName('test');
+        $this->assertTrue('test' == $this->product->__toString());
     }
     
     public function testAddCategory()
     {
-        $product = new Product();
-        $categories = $product->getCategories();
+        $categories = $this->product->getCategories();
         
         $this->assertTrue($categories instanceOf \Doctrine\Common\Collections\ArrayCollection);
         $this->assertTrue(0 === $categories->count());
         
         $mockCategory = $this->getMock('\eStore\ShopBundle\Entity\Category');
-        $product->addCategory($mockCategory);
+        $this->product->addCategory($mockCategory);
         
+        $categories = $this->product->getCategories();
         $this->assertTrue(1 === $categories->count());
     }
 }
